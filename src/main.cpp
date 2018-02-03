54eb23d88c7e432abb251cb8e36d886b6b9af2df
Matej Odalos <nilbeleth@valec.net>
2018-02-04 00:51:21 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "config.h"
+
+
+
+using namespace std;
+
+
+int main(int argc, char const* argv[])
+{
+    cout << "Hello world! - " << Tutorial_VERSION_MAJOR << endl;
+}
