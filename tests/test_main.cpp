73cc37659267ef00d7c64b06228d214278d9bdda
Matej Odalos <nilbeleth@valec.net>
2018-07-23 23:20:50 +0200

    - some crazy stuff trying to unit tests to work

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -4,18 +4,23 @@
  * @license   MIT, see the @c LICENSE file for more details
  * @brief     Simple Hello world!!! application.
  */
-#include <catch.hpp>
+#include <catch2/catch.hpp>
 
 #define CATCH_CONFIG_MAIN
 
 
 namespace sample-project
 {
+
+    unsigned int Factorial( unsigned int number ) {
+        return number <= 1 ? number : Factorial(number - 1)*number;
+    }
+
 namespace tests
 {
     TEST_CASE( "Sem netusom co mam dat", "[funkcia]")
     {
-        REQUIRE( 1 == 2 );
+        REQUIRE( Factorial(0) == 1 );
     }
 }   // ned namespace tests
 }   // end namespace sample-project
