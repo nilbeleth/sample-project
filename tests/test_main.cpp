84b5e229613932f772f81efa936bdf0874b24470
Matej Odalos <nilbeleth@valec.net>
2018-07-27 23:29:39 +0200

    - working unit tests; main.cpp is not possible to test so far

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -2,25 +2,8 @@
  * @file      test_main.cpp
  * @copyright (c) 2018 by Matej Odalos <nilbeleth@valec.net>
  * @license   MIT, see the @c LICENSE file for more details
- * @brief     Simple Hello world!!! application.
+ * @brief     A unit test for src/main.cpp
  */
-#include <catch2/catch.hpp>
-
 #define CATCH_CONFIG_MAIN
 
-
-namespace sample-project
-{
-
-    unsigned int Factorial( unsigned int number ) {
-        return number <= 1 ? number : Factorial(number - 1)*number;
-    }
-
-namespace tests
-{
-    TEST_CASE( "Sem netusom co mam dat", "[funkcia]")
-    {
-        REQUIRE( Factorial(0) == 1 );
-    }
-}   // ned namespace tests
-}   // end namespace sample-project
+#include <catch2/catch.hpp>
