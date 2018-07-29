313464edf56dac43f144e1820f2b4fa3af2d8247
Matej Odalos <nilbeleth@valec.net>
2018-07-29 20:24:52 +0000

    Review-new: Implement unittests

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,24 @@
-/**
- * @file      main.cpp
- * @copyright (c) 2018 by Matej Odalos <nilbeleth@valec.net>
- * @license   MIT, see the @c LICENSE file for more details
- * @brief     Simple Hello world!!! application.
- */
+/*****************************************************************************\
+ * Project:   sample-project
+ * Copyright: (c) 2018 by Matej Odalos <nilbeleth@valec.net>
+ * License:   MIT, see the LICENSE file for more details
+ *
+ * Simple Hello world!!! application.
+ *
+\*****************************************************************************/
 #include <iostream>
 #include <cstdlib>
 #include "version.h"
-
+#include "main.h"
 
 using namespace std;
 
 
-/**
- * @brief Entry point of application.
- * @param[in] argc Number of command line arguments.
- * @param[in] argv Command line passed to application.
- * @return @c returns true if application finishes without any error,
- *         @c false otherwise/
- *
- * This application actually does nothing besides printing simple message.
- *
- */
 int main(int argc, char const* argv[])
 {
-    (void) argc;
-    (void) argv;
-    cout << "Hello world! - " << Version::getBuildMachine() << endl;
+  (void) argc;
+  (void) argv;
+  cout << "Hello world!" << endl;
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
